a058fd15fac52f3b5f5c58c87367a133d307b413
DESKTOP-7AB9G8D\Geert <g.j.haans@students.uu.nl>
2023-11-20 22:24:02 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.login(process.env.BOT_PASSWORD);
