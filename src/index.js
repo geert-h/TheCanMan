12b635fe75f1fe62ebe4851ba7f6dcd24daf3659
DESKTOP-7AB9G8D\Geert <g.j.haans@students.uu.nl>
2023-11-20 23:21:14 +0100

    worked on the basics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
-    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
   ],
 });
 
-client.login(process.env.BOT_PASSWORD);
+client.on("ready", (c) => {
+  console.log(`✅ ${c.user.tag} is online`);
+});
+
+client.on("messageCreate", (message) => {
+  console.log(message);
+
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "hello") {
+    message.reply("Hey!");
+  }
+});
+
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "hey") {
+    interaction.reply("hey!");
+  }
+
+  if (interaction.commandName === "ping") {
+    interaction.reply("Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
